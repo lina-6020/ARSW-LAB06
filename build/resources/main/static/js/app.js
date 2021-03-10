ce1ccb06aa0d9966dbfc9c79fec38a8c7fa46944
Johann Bogota <johann.bogota@mail.escuelaing.edu.co>
2021-03-09 19:55:46 -0500

    Implementacion apiclient

--- build/resources/main/static/js/app.js
+++ build/resources/main/static/js/app.js
@@ -1,11 +1,13 @@
-var mock = apimock;
+//var mock = apimock;
+var api = apiclient;
 
 var handlerBlueprints = (function () {
   var author = null;
   var list = [];
 
-  var setName = function (newAuthor) {
+  var _setName = function (newAuthor) {
     author = newAuthor;
+    $("#campo2").text(`${author}'s blueprints`);
   };
 
   var actualizar = function (blueprintsByAuthor) {
@@ -22,40 +24,41 @@ var handlerBlueprints = (function () {
       var total = listBlueprints.reduce((a, b) => a + b.points, 0);
 
       $("#totalPoints").text("Total points: " + total);
+      $("#blueprints tbody").empty();
       listBlueprints.map((blueprint) =>
-        $("#blueprints tbody").append(`<tr><td>${blueprint.name}</td>
-                                    <td>${blueprint.points}</td>
-                                    <td><button type="button" onClick="handlerBlueprints.getPoints('
-                                    ${blueprint.name}'
-                                    ) ">Open</button></td><tr/>`)
+        $("#blueprints tbody").append(
+          `<tr><td>${blueprint.name}</td>
+      <td>${blueprint.points}</td>
+      <td><button type="button" onClick='handlerBlueprints.getPoints("${blueprint.name}")'>Open</button></td></tr>`
+        )
       );
     }
   };
 
   var updateBlueprints = function (sendAuthor) {
-    author = sendAuthor;
-    apimock.getBlueprintsByAuthor(author, actualizar);
+    _setName(sendAuthor);
+    api.getBlueprintsByAuthor(sendAuthor, actualizar);
+    //apimock.getBlueprintsByAuthor(author, actualizar);
   };
 
   var putPoints = function (listPoints) {
-    console.log("sdfsfsdf");
     $("#canvasTitle").text("Current blueprint: " + listPoints.name);
     var myCanvas = document.getElementById("myCanvas");
     var contx = myCanvas.getContext("2d");
     contx.clearRect(0, 0, myCanvas.width, myCanvas.height);
     contx.beginPath();
     var first = listPoints.points[0];
-    ctx.moveTo(first.x, first.y);
+    contx.moveTo(first.x, first.y);
     listPoints.points.map((point) => contx.lineTo(point.x, point.y));
-    ctx.stroke();
+    contx.stroke();
   };
 
   var getPoints = function (nameBlueprints) {
-    apimock.getBlueprintsByNameAndAuthor(author, nameBlueprints, putPoints);
+    api.getBlueprintsByNameAndAuthor(author, nameBlueprints, putPoints);
+    //apimock.getBlueprintsByNameAndAuthor(author, nameBlueprints, putPoints);
   };
 
   return {
-    setName: setName,
     updateBlueprints: updateBlueprints,
     getPoints: getPoints,
   };
